cadef07286a78895cc2864d29267f918c63a9166
Kirk Rohani <kirk.rohani@gmail.com>
2023-07-02 00:34:11 -0500

    fixed missing text by adding style to main View

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View>
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
       <Text>Welcome to Search AI!</Text>
       <StatusBar style="auto" />
     </View>
